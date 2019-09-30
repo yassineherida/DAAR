f19154829c6e7441f446b736c1ec043676b032dc
EYRAUD <3535008@ppti-14-507-04.ufr-info-p6.jussieu.fr>
2019-09-30 18:10:48 +0200

    tp3

--- Automate.java
+++ Automate.java
@@ -1,10 +1,28 @@
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Pattern;
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.toMap;
+
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
+
 
 public class Automate {
 
@@ -395,6 +413,61 @@ public class Automate {
 		 }
 	 }
 	 
+
+	 
+	 public static void indexing (String fileName) throws IOException {
+	        InputStream flux=new FileInputStream(fileName); 
+	        InputStreamReader lecture=new InputStreamReader(flux);
+	        BufferedReader buff=new BufferedReader(lecture);
+	        Map<String, ArrayList<Coordinates>> index = new HashMap<String, ArrayList<Coordinates>>();
+	        String ligne;
+	        int c=0;
+	        int i = 1, j = 1;
+	        while ((ligne=buff.readLine())!=null){
+	        	j=1;
+	        	String[] words = ligne.split("\\s*[^a-zA-Z'-]+\\s*");
+	        	int cpt = 0;
+	        	for (cpt=0;cpt<words.length;cpt++) {
+		        	String word = words[cpt];
+		        	word = word.toLowerCase();
+		        	//word = word.toLowerCase();
+	        		if(index.containsKey(word)) {
+	        			index.get(word).add(new Coordinates(i, j));
+	        		}
+	        		else {
+	        			index.put(word, new ArrayList<>());
+	        			index.get(word).add(new Coordinates(i, j));
+	        		}
+	        		int space_count = 0;
+	        		j += word.length();
+	        		if(cpt < words.length-1) 
+		        		while(ligne.charAt(j-1) != words[cpt+1].charAt(0)) {
+		        			j++;
+		        		}
+	        	}
+	        	i++;
+	        }
+	        System.out.println(index.get("sargon's").size());
+	        HashMap<String, ArrayList<Coordinates>> sorted = index.entrySet().stream()
+	        	    .sorted(comparingInt(e -> e.getValue().size()))
+	        	    .collect(toMap(
+	        	        Map.Entry::getKey,
+	        	        Map.Entry::getValue,
+	        	        (a, b) -> { throw new AssertionError(); },
+	        	        LinkedHashMap::new
+	        	    ));
+	        sorted.remove("");
+	       
+	        for(Entry<String, ArrayList<Coordinates>> e:sorted.entrySet()) {
+	    
+	        	//System.out.println(e.getKey() + " " + e.getValue());
+	        }
+	        
+	 }
+	 
+	 
+	 
+	 
 	 public static void main(String arg[]) {
 		 //RegExTree a = new RegExTree((int)'a', new ArrayList<RegExTree>());
 		 //RegExTree b = new RegExTree((int)'b', new ArrayList<RegExTree>());
@@ -412,13 +485,20 @@ public class Automate {
 		 //System.out.println(test.etatF);
 		 //System.out.println(test.parcours("bcaa aa b aaa ab"));
 		 
+		 /*
 		 int[] t = Automate.retenue("mamamamia");
 		 for (int e : t)
 			 System.out.println(e);
 
 	      char[] facteur = new String("mamamamia").toCharArray();
 	      char[] txt = new String("memamamamia").toCharArray();
-	 		System.out.println(Automate.match(facteur, t, txt));
+	 		System.out.println(Automate.match(facteur, t, txt));*/
+		 try {
+			indexing("src/test2.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	 
 	 }
 }
