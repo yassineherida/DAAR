60c5cebfe36b3cf837dc74b1dfb39e5cc9be356d
EYRAUD <3535008@ppti-14-507-02.ufr-info-p6.jussieu.fr>
2019-10-04 18:08:02 +0200

    radix

---
+++ TME1/src/TME1.java
@@ -0,0 +1,15 @@
+
+public class TME1 {
+	
+	public int[] retenue (String pattern) {
+		
+		int[] retenue = new int[pattern.length()];
+		int i = 0;
+		int j = -1;
+		if(pattern[i] == pattern[j]) {
+			
+		}
+		
+		return retenue;
+	}
+}
