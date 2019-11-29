f069fb64ca7febfd25d95d898e158c6a1d7623dc
yassineherida <yassineherida@gmail.com>
2019-11-29 22:08:40 +0100

    Update Closeness.java

--- TME4_DAAR/src/Closeness.java
+++ TME4_DAAR/src/Closeness.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
-
 public class Closeness {
 	public double[] closeness(int[][] dists) {
 		double[] res = new double[dists.length];
-		
+		double dist =0.0;
+		for (int i = 0;i<dists.length;i++) {
+			for (int j =0;j<dists.length;i++) {
+				dist = dist + dists[i][j];
+			}
+			res[i]=dists.length/dist;
+			dist=0.0;
+		}
 		return res;
 	}
 }
